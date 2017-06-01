34b3cfed58c77bf3e57c3ac6d708540f656bc622
HarryU <hmucs@yahoo.co.uk>
2017-06-01 17:52:31 +0200

    adding animation to css class changing

--- html/js/class_change_by_time.js
+++ html/js/class_change_by_time.js
@@ -1,12 +1,24 @@
 var d = new Date();
 var n = d.getHours();
 if (n > 19 || n < 6) {
-    document.body.className = "night";
+    document.body.className = "night animated";
+		document.getElementById("bg").classList.add("night");
+		document.getElementById("bg").classList.remove("day");
 		document.getElementById("mainbox").classList.add("night");
 		document.getElementById("mainbox").classList.remove("day");
+    var buttons =document.getElementsByName("button");
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].classList.remove("day");
+			buttons[i].classList.add("night");
+		}
 }
 else {
     document.body.className = "day";
 		document.getElementById("mainbox").classList.remove("night");
 		document.getElementById("mainbox").classList.add("day");
+    var buttons =document.getElementsByName("button");
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].classList.remove("night");
+			buttons[i].classList.add("day");
+		}
 }
