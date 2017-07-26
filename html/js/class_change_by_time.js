c2ed2e7ba90911d714a989c6cdf6efc3c65705dd
HarryU <hmucs@yahoo.co.uk>
2017-07-26 13:47:57 +0200

    adding sub game and changing homepage somewhat

--- html/js/class_change_by_time.js
+++ html/js/class_change_by_time.js
@@ -1,7 +1,8 @@
 var d = new Date();
 var n = d.getHours();
 if (n > 19 || n < 6) {
-    document.body.className = "night animated";
+    document.body.className = "night";
+		document.getElementById("bg").classList.add("animated");
 		document.getElementById("bg").classList.add("night");
 		document.getElementById("bg").classList.remove("day");
 		document.getElementById("mainbox").classList.add("night");
@@ -14,6 +15,8 @@ if (n > 19 || n < 6) {
 }
 else {
     document.body.className = "day";
+		document.getElementById("bg").classList.remove("animated");
+		document.getElementById("bg").classList.remove("night");
 		document.getElementById("mainbox").classList.remove("night");
 		document.getElementById("mainbox").classList.add("day");
     var buttons =document.getElementsByName("button");
