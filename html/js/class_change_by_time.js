11466fe8a1239d5b760eae02e31459694b0e4b6f
host <host@host.lan>
2018-02-19 11:25:00 +0000

    weekend updates - moving previous homepage and adding resume as primary page

--- html/js/class_change_by_time.js
+++ html/js/class_change_by_time.js
@@ -1,13 +1,12 @@
 var d = new Date();
 var n = d.getHours();
-if (n > 19 || n < 6) {
+if (n > 18 || n < 6) {
     document.body.className = "night";
-		document.getElementById("bg").classList.add("animated");
 		document.getElementById("bg").classList.add("night");
 		document.getElementById("bg").classList.remove("day");
 		document.getElementById("mainbox").classList.add("night");
 		document.getElementById("mainbox").classList.remove("day");
-    var buttons =document.getElementsByName("button");
+    var buttons = document.getElementsByName("button");
 		for (var i = 0; i < buttons.length; i++) {
 			buttons[i].classList.remove("day");
 			buttons[i].classList.add("night");
@@ -15,8 +14,8 @@ if (n > 19 || n < 6) {
 }
 else {
     document.body.className = "day";
-		document.getElementById("bg").classList.remove("animated");
 		document.getElementById("bg").classList.remove("night");
+		document.getElementById("bg").classList.add("day");
 		document.getElementById("mainbox").classList.remove("night");
 		document.getElementById("mainbox").classList.add("day");
     var buttons =document.getElementsByName("button");
